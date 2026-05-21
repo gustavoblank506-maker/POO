feff9bf3f84bf0c165f023e4e3daa8bdcee33894
Aluno Alagoinhas <aluno.alagoinhas@senaibahia.com.br>
2026-05-21 20:43:34 -0300

    Adição de objeto e metódo

--- Pessoa.js
+++ Pessoa.js
@@ -2,11 +2,12 @@
 class Pessoa {
 
     //método construtor - atributos da classe
-    constructor(nome, idade, cpf, cidade) {
+    constructor(nome, idade, cpf, profissao, cidade) {
         this.nome = nome;
         this.idade = idade;
         this.cpf = cpf;
         this.cidade = cidade;
+        this.profissao = profissao;
     }
 
     //método da classe - ação da classe
@@ -18,10 +19,13 @@ class Pessoa {
         this.idade++;
         console.log(`Parabéns ${this.nome}!, agora você tem ${this.idade} `);
     }
+    consultarProfissao(){
+        console.log(`Profissão: ${this.profissao}`);
+    }
 }
 
 //instanciando objetos da classe Pessoa
-const pessoa1 = new Pessoa("Marcos", 32, "30218625-78", "Alagoinhas");
+const pessoa1 = new Pessoa("Marcos", 32, "30218625-78","Professor", "Alagoinhas");
 
 //mostrando o objeto completo
 console.log(pessoa1);
@@ -31,9 +35,11 @@ pessoa1.fazerAniversario();
 //chamando o método do objeto
 pessoa1.mostrarDetalhes();
 
+pessoa1.consultarProfissao();
+
 console.log("-------------------");
 
-const pessoa2 = new Pessoa("Maria", 30, "022018325-18", "Barreiras");
+const pessoa2 = new Pessoa("Maria", 30, "022018325-18","Professora", "Barreiras");
 
 //mostrando o objeto completo
 console.log(pessoa2);
@@ -44,3 +50,5 @@ pessoa2.mostrarDetalhes();
 pessoa2.fazerAniversario();
 
 pessoa2.mostrarDetalhes();
+
+pessoa2.consultarProfissao();
